412c8a9227289b0de8c40e986df661419263d890
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2023-12-24 21:50:28 +0100

    En la clase Equipo se ha realizado una corrección al metodo getNumJugadores() para que devuelva el numero exacto de jugadores en el equipo en vez del máximo permitido.
    Y de la misma clase ya se han incluido los metodos "mostrarListaJugadores()" y "adquirirJugador(Jugador jugador)".

--- Equipo.java
+++ Equipo.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 public class Equipo {
     private String Nombre;
     private String Ciudad;
@@ -22,6 +24,22 @@ public class Equipo {
 
     //Metodo que nos permite conocer el numero de jugadores que compone el equipo
     public int getNumJugadores() {
-        return NUMMAXJUGADORES;
+        return ListaJugadores.length;
     }
+
+    public void mostrarListaJugadores(){
+        for (int i= 0; i < ListaJugadores.length; i++){
+            System.out.println(ListaJugadores[i].getNombre());
+        }
+    }
+
+    public void adquirirJugador(Jugador jugador){
+        int i = 0;
+        while (ListaJugadores[i] != null){
+            i++;
+        }
+        ListaJugadores[i] = jugador;
+    }
+
+
 }
